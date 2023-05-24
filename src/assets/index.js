dd8110832e7a0548605b93625a105e62f939f283
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-25 02:21:36 +0800

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,74 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import swipesetup from "./swipesetup.png";
+import reactWeatherApp from "./reactweatherapp.png";
+import uc from "./company/uc.png";
+import gs from "./company/gs.png";
+import ga from "./company/ga.png";
+import socketio from "./tech/socketio.png";
+import chakraui from "./tech/chakra.png";
+import mernchatapp from "./mernchatapp.png";
+import netlify from "./netlify.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  uc,
+  gs,
+  ga,
+  socketio,
+  chakraui,
+  mernchatapp,
+  swipesetup,
+  netlify,
+  reactWeatherApp,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,275 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  swipesetup,
+  threejs,
+  uc,
+  gs,
+  ga,
+  chakraui,
+  socketio,
+  mernchatapp,
+  reactWeatherApp,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "React Developer",
+    icon: web,
+  },
+  {
+    title: "Frontend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Fullstack Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "socket.io",
+    icon: socketio,
+  },
+  {
+    name: "chakraui",
+    icon: chakraui,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full Stack Developer",
+    company_name: "General Assembly Singapore",
+    icon: ga,
+    iconBg: "#E6DEDD",
+    date: "Nov 2022 - May 2023",
+    points: [
+      "Developed a simple realtime chat application made using React JS, a JavaScript library for building user interfaces by Facebook, Node JS, Express JS, Socket.io, and MongoDB",
+      "Collaborating with other developers on creating a rating and review website for computer desk setups using MongoDB, Express, React and Node ( https://swipe-setups.netlify.app/ )",
+      "Developed a frontend weather app that utilizes the OpenWeatherMap API to fetch and display weather data to users ( https://react-weather-app-two-kappa.vercel.app/ )",
+      "Developed a typing game that challenges players with mini typing tests, improving their keyboarding speed and accuracy ( https://weilianglee87.github.io/simple-typing-game/ )",
+    ],
+  },
+  {
+    title: "Projects Manager",
+    company_name: "GS Media",
+    icon: gs,
+    iconBg: "#383E56",
+    date: "Nov 2017 - Present",
+    points: [
+      "Experienced in overseeing event planning, coordination, budgeting, vendor management, and client relations",
+      "Proven track record of successfully executing diverse events",
+      "Skilled in developing profitable business strategies",
+      "Proficient in managing teams and optimizing budgets",
+      "Implementing effective digital strategies to enhance event outcomes",
+    ],
+  },
+  {
+    title: "Event Production Supervisor",
+    company_name: "Urban Creations Pte Ltd",
+    icon: uc,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I had the pleasure of collaborating with Wei Liang on our full-stack MERN project at GA. Wei Liang's remarkable analytical and logical dexterity were key in implementing complex sorting and filtering algorithms. His attention to detail, problem-solving abilities, and contributions significantly enhanced our app's user experience, particularly in relation to products, ratings, and reviews. I have full confidence that he will continue to excel in future projects.",
+    name: "Alvin Lee",
+    designation: "Founder ",
+    company: "Omniv3rse",
+    image:
+      "https://media.licdn.com/dms/image/D5603AQGn2G7mYZfGMA/profile-displayphoto-shrink_800_800/0/1676371323564?e=1690416000&v=beta&t=2j36mP8KN7inC_1qU_NIpZsseYt09kCuY62PxUfNunU",
+  },
+  {
+    testimonial:
+      "Wei Liang is a highly skilled and talented full stack developer who works effectively with his classmates. Throughout our time studying together, Wei Liang consistently impressed us with his technical prowess in Javascript and ReactJS, and collaborative abilities. Given his exceptional work ethic and expertise in the field, I am confident that Wei Liang will be a successful developer. He will be a valuable asset for anyone in search of a dedicated and skilled team player.",
+    name: "Irvinn Lim",
+    designation: "Student ",
+    company: "General Assembly",
+    image:
+      "https://media.licdn.com/dms/image/D5603AQFw-UsxhtOiVw/profile-displayphoto-shrink_800_800/0/1672741334061?e=1690416000&v=beta&t=dyrQeKdQJCYRAZ5xx7kYIxQgCsJO95tCz513OdLDcB0",
+  },
+];
+
+const projects = [
+  {
+    name: "Mern Chat App",
+    description:
+      "Developed a simple realtime chat application made using React JS, a JavaScript library for building user interfaces by Facebook, Node JS, Express JS, Socket.io, and MongoDB",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "chakraui",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "socket.io",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Express",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Nodejs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: mernchatapp,
+    newIcon: false,
+    source_code_link: "https://github.com/weilianglee87/mern-chat-app",
+  },
+  {
+    name: "swipe-setups",
+    description:
+      "Collaborating with other developers on creating a rating and review website for computer desk setups using MongoDB, Express, React and Node",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "chakraui",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Express",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Nodejs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: swipesetup,
+    newIcon: true,
+    source_code_link: "https://swipe-setups.netlify.app/",
+  },
+  {
+    name: "React-Weather-App",
+    description:
+      "Developed a frontend weather app that utilizes the OpenWeatherMap API to fetch and display weather data to users",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: reactWeatherApp,
+    newIcon: false,
+    source_code_link: "https://github.com/weilianglee87/React-Weather-App",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
