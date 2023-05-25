33b6207329be13d382fdf0e3c4040a56b36132e3
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-25 17:20:47 +0800

    bug fix hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,15 +26,16 @@ const Hero = () => {
       <ComputersCanvas />
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'></div>
-          <motion.div
-            animate={{
-              duration: 1.5,
-              repeat: Infinity,
-              repeatType: "loop",
-            }}
-            className='w-3 h-3 rounded-full bg-secondary mb-1'
-          />
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
         </a>
       </div>
     </section>
