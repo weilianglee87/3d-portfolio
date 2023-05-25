c3af2083f13928391a1b18d2b4d52e43ee0f50e4
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-25 18:06:01 +0800

    edit the gap in testimonials

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -43,14 +43,16 @@ const FeedbackCard = ({
 
 const Feedbacks = () => {
   return (
-    <div className='mt-12 bg-black-100 rounded-[20px]'>
-      <div className={`${styles.padding}bg-tertiary rounded-2xl min-h-[300px]`}>
+    <div className='mt-2 bg-black-100 rounded-[20px]'>
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[100px]`}
+      >
         <motion.div variants={textVariant()}>
           <p className={styles.sectionSubText}>What others say</p>
           <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
       </div>
-      <div className={`${styles.paddingX} mt-20 pb-14 flex flex-wrap gap-1`}>
+      <div className={`${styles.paddingX} mt-2 pb-2 flex flex-wrap gap-5`}>
         {testimonials.map((testimonial, index) => (
           <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
         ))}
