01adcecd2dba2ef9114362c885c1bf4ccc150a83
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-26 17:29:33 +0800

    viewport amount to 0

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,6 +1,6 @@
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import { staggerContainer } from '../utils/motion';
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { staggerContainer } from "../utils/motion";
 
 const SectionWrapper = (Component, idName) =>
   function HOC() {
@@ -9,7 +9,7 @@ const SectionWrapper = (Component, idName) =>
         variants={staggerContainer()}
         initial='hidden'
         whileInView='show'
-        viewport={{ once: true, amount: 0.25 }}
+        viewport={{ once: true, amount: 0 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
         <span className='hash-span' id={idName}>
