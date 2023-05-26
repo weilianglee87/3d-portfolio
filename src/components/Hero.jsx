90d2174603306a65c588532bb8edab0c4f1aca77
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-05-26 22:05:53 +0800

    fix bug for float32array to 5001 and motion animate in herojsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,6 @@
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
-
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
@@ -29,6 +27,9 @@ const Hero = () => {
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
                 duration: 1.5,
                 repeat: Infinity,
                 repeatType: "loop",
